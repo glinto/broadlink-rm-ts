b204ad98eb857aed945db2aaabc656b78e986fb3
glinto <glinto@gmail.com>
2021-11-28 04:53:28 +0100

    Port device functionality

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const crypto = require('crypto');
 const assert = require('assert');
 */
 
+import { createCipheriv, createDecipheriv } from "crypto";
 import { createSocket, RemoteInfo, Socket } from "dgram";
 import EventEmitter from "events";
 import { NetworkInterfaceInfo, networkInterfaces } from "os";
@@ -70,6 +71,9 @@ export class Broadlink extends EventEmitter {
 		this.sockets = [];
 	}
 
+	/**
+	 * Look for supported devices on the local network
+	 */
 	discover() {
 		// Close existing sockets
 		this.sockets.forEach((socket) => {
@@ -97,6 +101,11 @@ export class Broadlink extends EventEmitter {
 		});
 	}
 
+	/**
+	 * Gets all non-internal IPV4 addresses
+	 * 
+	 * @returns List of IP addresses
+	 */
 	private getIPAddresses(): string[] {
 		let interfaces = networkInterfaces();
 		let ipAddresses: string[] = [];
@@ -121,7 +130,7 @@ export class Broadlink extends EventEmitter {
 
 		let splitIPAddress = ipAddress.split('.');
 		const port = socket.address().port;
-		console.log(`\x1b[35m[INFO]\x1b[0m Listening for Broadlink devices on ${ipAddress}:${port} (UDP)`);
+		log(`Listening for Broadlink devices on ${ipAddress}:${port} (UDP)`);
 
 		let now = new Date();
 		let starttime = now.getTime();
@@ -191,296 +200,342 @@ export class Broadlink extends EventEmitter {
 
 		let deviceType = message[0x34] | (message[0x35] << 8);
 
-		console.log(`\x1b[35m[INFO]\x1b[0m Discovered Broadlink device with MAC address ${macAddress} and type ${deviceType}`);
+		log(`Discovered Broadlink device with MAC address ${macAddress} and type ${deviceType}`);
 
 		// Create a Device instance
-		this.addDevice(host, macAddress, deviceType);
+		this.addDevice(host, macAddressBuffer, deviceType);
 	}
 
 
-	addDevice(host: RemoteInfo, macAddress: string, deviceType: number) {
+	addDevice(host: RemoteInfo, mac: Buffer, deviceType: number) {
 
 		// If the device has already been added, ignore it
-		if (this.devices[macAddress]) return;
+		if (this.devices[mac.toString('hex')]) return;
 
 		// Ignore devices that don't support infrared or RF.
 		if (unsupportedDeviceTypes[deviceType] !== undefined) {
-			console.log(`\x1b[35m[INFO]\x1b[0m Ignoring unsupported device type ${unsupportedDeviceTypes[deviceType]} at ${host.address}:${host.port}`);
+			(`Ignoring unsupported device type ${unsupportedDeviceTypes[deviceType]} at ${host.address}:${host.port}`);
 			return;
 		}
 		// Ignore OEM branded SPMini2
 		if (deviceType >= 0x7530 && deviceType <= 0x7918) {
-			console.log(`\x1b[35m[INFO]\x1b[0m Ignoring unsupported device type ${deviceType} at ${host.address}:${host.port}`);
+			log(`Ignoring unsupported device type ${deviceType} at ${host.address}:${host.port}`);
 			return;
 		}
 
 		// Ignore unknown device types
-		if (rmDeviceTypes[deviceType] == undefined || rmPlusDeviceTypes[deviceType] == undefined) {
-			console.log(`\x1b[35m[INFO]\x1b[0m Ignoring unknown device type ${deviceType} at ${host.address}:${host.port}`);
+		if (rmDeviceTypes[deviceType] == undefined && rmPlusDeviceTypes[deviceType] == undefined) {
+			log(`Ignoring unknown device type ${deviceType.toString(16)} at ${host.address}:${host.port}`);
 			return;
 		}
 
-		let deviceName = rmDeviceTypes[deviceType] || rmPlusDeviceTypes[deviceType];
-		console.log(`\x1b[35m[INFO]\x1b[0m Found ${deviceName} at ${host.address}:${host.port}`);
+		let device = new Device(host, mac, deviceType);
+		this.devices[mac.toString('hex')] = device;
 
-		/*const device = new Device(host, macAddress, deviceType)
-
-		this.devices[macAddress] = device;
+		log(`Found ${device.model} at ${host.address}:${host.port}`);
 
 		// Authenticate the device and let others know when it's ready.
 		device.on('deviceReady', () => {
 			this.emit('deviceReady', device);
 		});
 
-		device.authenticate();*/
+		device.authenticate();
+	}
+
+	// Event overloads with typed listener signatures
+	on(event: 'deviceReady', listener: (device: Device) => void): this;
+	on(event: string | symbol, listener: (...args: any[]) => void): this;
+	on(event: string | symbol, listener: (...args: any[]) => void): this {
+		return super.on(event, listener);
 	}
 }
 
 
-class Device {
+class Device extends EventEmitter {
+
+	readonly host: RemoteInfo;
+	readonly mac: Buffer;
+	readonly type: number;
+	readonly model: string;
+	readonly socket: Socket;
+	count: number;
+
+	key: Buffer;
+	iv: Buffer;
+	id: Buffer;
+
+
+	constructor(host: RemoteInfo, mac: Buffer, type: number) {
+		super();
+		this.host = host;
+		this.mac = mac;
+
+		this.type = type;
+		this.model = rmDeviceTypes[type] || rmPlusDeviceTypes[type];
+
+		this.socket = createSocket({ type: 'udp4', reuseAddr: true });
+
+		this.count = Math.random() & 0xffff;
+		this.key = Buffer.from([0x09, 0x76, 0x28, 0x34, 0x3f, 0xe9, 0x9e, 0x23, 0x76, 0x5c, 0x15, 0x13, 0xac, 0xcf, 0x8b, 0x02]);
+		this.iv = Buffer.from([0x56, 0x2e, 0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58]);
+		this.id = Buffer.from([0, 0, 0, 0]);
+
+		this.listen();
+
+
+		//const isRFSupported = rmPlusDeviceTypes[type];
+		//if (isRFSupported) this.addRFSupport();
+	}
+
+
+	/**
+	 * Start listening on the device socket
+	 */
+	listen() {
+
+		this.socket.on('message', (response) => {
+			let encryptedPayload = Buffer.alloc(response.length - 0x38, 0);
+			response.copy(encryptedPayload, 0, 0x38);
+
+			let err = response[0x22] | (response[0x23] << 8);
+			if (err != 0) return;
+
+			let decipher = createDecipheriv('aes-128-cbc', this.key, this.iv);
+			decipher.setAutoPadding(false);
+
+			let payload = decipher.update(encryptedPayload);
+
+			let p2 = decipher.final();
+			if (p2) payload = Buffer.concat([payload, p2]);
+
+			if (!payload) return false;
+
+			let command = response[0x26];
+
+			if (command == 0xe9) {
+				this.key = Buffer.alloc(0x10, 0);
+				payload.copy(this.key, 0, 0x04, 0x14);
+
+				this.id = Buffer.alloc(0x04, 0);
+				payload.copy(this.id, 0, 0x00, 0x04);
+
+				this.emit('deviceReady', this);
+			} else if (command == 0xee || command == 0xef) {
+				this.onPayloadReceived(err, payload);
+			} else {
+				log(`Unknown command received: 0x${command.toString(16)}`);
+			}
+		});
+
+		this.socket.bind();
+	}
+
+
+	authenticate() {
+		let payload = Buffer.alloc(0x50, 0);
+
+		payload[0x04] = 0x31;
+		payload[0x05] = 0x31;
+		payload[0x06] = 0x31;
+		payload[0x07] = 0x31;
+		payload[0x08] = 0x31;
+		payload[0x09] = 0x31;
+		payload[0x0a] = 0x31;
+		payload[0x0b] = 0x31;
+		payload[0x0c] = 0x31;
+		payload[0x0d] = 0x31;
+		payload[0x0e] = 0x31;
+		payload[0x0f] = 0x31;
+		payload[0x10] = 0x31;
+		payload[0x11] = 0x31;
+		payload[0x12] = 0x31;
+		payload[0x1e] = 0x01;
+		payload[0x2d] = 0x01;
+		payload[0x30] = 'T'.charCodeAt(0);
+		payload[0x31] = 'e'.charCodeAt(0);
+		payload[0x32] = 's'.charCodeAt(0);
+		payload[0x33] = 't'.charCodeAt(0);
+		payload[0x34] = ' '.charCodeAt(0);
+		payload[0x35] = ' '.charCodeAt(0);
+		payload[0x36] = '1'.charCodeAt(0);
+
+		this.sendPacket(0x65, payload);
+	}
+
+	sendPacket(command: number, payload: Buffer) {
+		this.count = (this.count + 1) & 0xffff;
+
+		let packet = Buffer.alloc(0x38, 0);
+
+		packet[0x00] = 0x5a;
+		packet[0x01] = 0xa5;
+		packet[0x02] = 0xaa;
+		packet[0x03] = 0x55;
+		packet[0x04] = 0x5a;
+		packet[0x05] = 0xa5;
+		packet[0x06] = 0xaa;
+		packet[0x07] = 0x55;
+		packet[0x24] = 0x2a;
+		packet[0x25] = 0x27;
+		packet[0x26] = command;
+		packet[0x28] = this.count & 0xff;
+		packet[0x29] = this.count >> 8;
+		packet[0x2a] = this.mac[5];
+		packet[0x2b] = this.mac[4];
+		packet[0x2c] = this.mac[3];
+		packet[0x2d] = this.mac[2];
+		packet[0x2e] = this.mac[1];
+		packet[0x2f] = this.mac[0];
+		packet[0x30] = this.id[0];
+		packet[0x31] = this.id[1];
+		packet[0x32] = this.id[2];
+		packet[0x33] = this.id[3];
+
+		let checksum = 0xbeaf;
+		for (let i = 0; i < payload.length; i++) {
+			checksum += payload[i];
+			checksum = checksum & 0xffff;
+		}
+
+		let cipher = createCipheriv('aes-128-cbc', this.key, this.iv);
+		payload = cipher.update(payload);
+
+		packet[0x34] = checksum & 0xff;
+		packet[0x35] = checksum >> 8;
+
+		packet = Buffer.concat([packet, payload]);
+
+		checksum = 0xbeaf;
+		for (let i = 0; i < packet.length; i++) {
+			checksum += packet[i];
+			checksum = checksum & 0xffff;
+		}
+		packet[0x20] = checksum & 0xff;
+		packet[0x21] = checksum >> 8;
+
+		//if (debug) log('\x1b[33m[DEBUG]\x1b[0m packet', packet.toString('hex'))
+
+		this.socket.send(packet, 0, packet.length, this.host.port, this.host.address, (err, bytes) => {
+			//if (debug && err) log('\x1b[33m[DEBUG]\x1b[0m send packet error', err)
+			//if (debug) log('\x1b[33m[DEBUG]\x1b[0m successfuly sent packet - bytes: ', bytes)
+		});
+	}
+
+	onPayloadReceived(err: number, payload: Buffer) {
+		let param = payload[0];
+
+		let data = Buffer.alloc(payload.length - 4, 0);
+		payload.copy(data, 0, 4);
+
+		switch (param) {
+			case 1: {
+				let temp = (payload[0x4] * 10 + payload[0x5]) / 10.0;
+				this.emit('temperature', temp);
+				break;
+			}
+			case 4: { //get from check_data
+				let data = Buffer.alloc(payload.length - 4, 0);
+				payload.copy(data, 0, 4);
+				this.emit('rawData', data);
+				break;
+			}
+			case 26: { //get from check_data
+				let data = Buffer.alloc(1, 0);
+				payload.copy(data, 0, 0x4);
+				if (data[0] !== 0x1) break;
+				this.emit('rawRFData', data);
+				break;
+			}
+			case 27: { //get from check_data
+				let data = Buffer.alloc(1, 0);
+				payload.copy(data, 0, 0x4);
+				if (data[0] !== 0x1) break;
+				this.emit('rawRFData2', data);
+				break;
+			}
+		}
+	}
+
+	// Externally Accessed Methods
+
+	checkData() {
+		const packet = Buffer.alloc(16, 0);
+		packet[0] = 4;
+		this.sendPacket(0x6a, packet);
+	}
+
+	sendData(data: Buffer) {
+		let packet = Buffer.from([0x02, 0x00, 0x00, 0x00]);
+		packet = Buffer.concat([packet, data]);
+		this.sendPacket(0x6a, packet);
+	}
+
+	enterLearning() {
+		let packet = Buffer.alloc(16, 0);
+		packet[0] = 3;
+		this.sendPacket(0x6a, packet);
+	}
+
+	checkTemperature() {
+		let packet = Buffer.alloc(16, 0);
+		packet[0] = 1;
+		this.sendPacket(0x6a, packet);
+	}
+
+	cancelLearn() {
+		const packet = Buffer.alloc(16, 0);
+		packet[0] = 0x1e;
+		this.sendPacket(0x6a, packet);
+	}
+
 	/*
-		constructor(host: RemoteInfo, macAddress: string, deviceType: number) {
-			this.host = host;
-			this.mac = macAddress;
-			this.emitter = new EventEmitter();
-			this.log = console.log;
-			this.type = deviceType;
-			this.model = rmDeviceTypes[parseInt(deviceType, 16)] || rmPlusDeviceTypes[parseInt(deviceType, 16)];
-	
-			this.on = this.emitter.on;
-			this.emit = this.emitter.emit;
-			this.removeListener = this.emitter.removeListener;
-	
-			this.count = Math.random() & 0xffff;
-			this.key = new Buffer([0x09, 0x76, 0x28, 0x34, 0x3f, 0xe9, 0x9e, 0x23, 0x76, 0x5c, 0x15, 0x13, 0xac, 0xcf, 0x8b, 0x02]);
-			this.iv = new Buffer([0x56, 0x2e, 0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58]);
-			this.id = new Buffer([0, 0, 0, 0]);
-	
-			this.setupSocket();
-	
-			// Dynamically add relevant RF methods if the device supports it
-			const isRFSupported = rmPlusDeviceTypes[parseInt(deviceType, 16)];
-			if (isRFSupported) this.addRFSupport();
-		}
-	
-		// Create a UDP socket to receive messages from the broadlink device.
-		setupSocket() {
-			const socket = dgram.createSocket({ type: 'udp4', reuseAddr: true });
-			this.socket = socket;
-	
-			socket.on('message', (response) => {
-				const encryptedPayload = Buffer.alloc(response.length - 0x38, 0);
-				response.copy(encryptedPayload, 0, 0x38);
-	
-				const err = response[0x22] | (response[0x23] << 8);
-				if (err != 0) return;
-	
-				const decipher = crypto.createDecipheriv('aes-128-cbc', this.key, this.iv);
-				decipher.setAutoPadding(false);
-	
-				let payload = decipher.update(encryptedPayload);
-	
-				const p2 = decipher.final();
-				if (p2) payload = Buffer.concat([payload, p2]);
-	
-				if (!payload) return false;
-	
-				const command = response[0x26];
-	
-				if (command == 0xe9) {
-					this.key = Buffer.alloc(0x10, 0);
-					payload.copy(this.key, 0, 0x04, 0x14);
-	
-					this.id = Buffer.alloc(0x04, 0);
-					payload.copy(this.id, 0, 0x00, 0x04);
-	
-					this.emit('deviceReady');
-				} else if (command == 0xee || command == 0xef) {
-					this.onPayloadReceived(err, payload);
-				} else {
-					console.log('Unhandled Command: ', command);
-				}
-			});
-	
-			socket.bind();
-		}
-	
-		authenticate() {
-			const payload = Buffer.alloc(0x50, 0);
-	
-			payload[0x04] = 0x31;
-			payload[0x05] = 0x31;
-			payload[0x06] = 0x31;
-			payload[0x07] = 0x31;
-			payload[0x08] = 0x31;
-			payload[0x09] = 0x31;
-			payload[0x0a] = 0x31;
-			payload[0x0b] = 0x31;
-			payload[0x0c] = 0x31;
-			payload[0x0d] = 0x31;
-			payload[0x0e] = 0x31;
-			payload[0x0f] = 0x31;
-			payload[0x10] = 0x31;
-			payload[0x11] = 0x31;
-			payload[0x12] = 0x31;
-			payload[0x1e] = 0x01;
-			payload[0x2d] = 0x01;
-			payload[0x30] = 'T'.charCodeAt(0);
-			payload[0x31] = 'e'.charCodeAt(0);
-			payload[0x32] = 's'.charCodeAt(0);
-			payload[0x33] = 't'.charCodeAt(0);
-			payload[0x34] = ' '.charCodeAt(0);
-			payload[0x35] = ' '.charCodeAt(0);
-			payload[0x36] = '1'.charCodeAt(0);
-	
-			this.sendPacket(0x65, payload);
-		}
-	
-		sendPacket(command, payload, debug = false) {
-			const { log, socket } = this;
-			this.count = (this.count + 1) & 0xffff;
-	
-			let packet = Buffer.alloc(0x38, 0);
-	
-			packet[0x00] = 0x5a;
-			packet[0x01] = 0xa5;
-			packet[0x02] = 0xaa;
-			packet[0x03] = 0x55;
-			packet[0x04] = 0x5a;
-			packet[0x05] = 0xa5;
-			packet[0x06] = 0xaa;
-			packet[0x07] = 0x55;
-			packet[0x24] = 0x2a;
-			packet[0x25] = 0x27;
-			packet[0x26] = command;
-			packet[0x28] = this.count & 0xff;
-			packet[0x29] = this.count >> 8;
-			packet[0x2a] = this.mac[5];
-			packet[0x2b] = this.mac[4];
-			packet[0x2c] = this.mac[3];
-			packet[0x2d] = this.mac[2];
-			packet[0x2e] = this.mac[1];
-			packet[0x2f] = this.mac[0];
-			packet[0x30] = this.id[0];
-			packet[0x31] = this.id[1];
-			packet[0x32] = this.id[2];
-			packet[0x33] = this.id[3];
-	
-			let checksum = 0xbeaf;
-			for (let i = 0; i < payload.length; i++) {
-				checksum += payload[i];
-				checksum = checksum & 0xffff;
-			}
-	
-			const cipher = crypto.createCipheriv('aes-128-cbc', this.key, this.iv);
-			payload = cipher.update(payload);
-	
-			packet[0x34] = checksum & 0xff;
-			packet[0x35] = checksum >> 8;
-	
-			packet = Buffer.concat([packet, payload]);
-	
-			checksum = 0xbeaf;
-			for (let i = 0; i < packet.length; i++) {
-				checksum += packet[i];
-				checksum = checksum & 0xffff;
-			}
-			packet[0x20] = checksum & 0xff;
-			packet[0x21] = checksum >> 8;
-	
-			if (debug) log('\x1b[33m[DEBUG]\x1b[0m packet', packet.toString('hex'))
-	
-			socket.send(packet, 0, packet.length, this.host.port, this.host.address, (err, bytes) => {
-				if (debug && err) log('\x1b[33m[DEBUG]\x1b[0m send packet error', err)
-				if (debug) log('\x1b[33m[DEBUG]\x1b[0m successfuly sent packet - bytes: ', bytes)
-			});
-		}
-	
-		onPayloadReceived(err, payload) {
-			const param = payload[0];
-	
-			const data = Buffer.alloc(payload.length - 4, 0);
-			payload.copy(data, 0, 4);
-	
-			switch (param) {
-				case 1: {
-					const temp = (payload[0x4] * 10 + payload[0x5]) / 10.0;
-					this.emit('temperature', temp);
-					break;
-				}
-				case 4: { //get from check_data
-					const data = Buffer.alloc(payload.length - 4, 0);
-					payload.copy(data, 0, 4);
-					this.emit('rawData', data);
-					break;
-				}
-				case 26: { //get from check_data
-					const data = Buffer.alloc(1, 0);
-					payload.copy(data, 0, 0x4);
-					if (data[0] !== 0x1) break;
-					this.emit('rawRFData', data);
-					break;
-				}
-				case 27: { //get from check_data
-					const data = Buffer.alloc(1, 0);
-					payload.copy(data, 0, 0x4);
-					if (data[0] !== 0x1) break;
-					this.emit('rawRFData2', data);
-					break;
-				}
-			}
-		}
-	
-		// Externally Accessed Methods
-	
-		checkData() {
+	addRFSupport() {
+		this.enterRFSweep = () => {
 			const packet = Buffer.alloc(16, 0);
-			packet[0] = 4;
+			packet[0] = 0x19;
 			this.sendPacket(0x6a, packet);
 		}
-	
-		sendData(data, debug = false) {
-			let packet = new Buffer([0x02, 0x00, 0x00, 0x00]);
-			packet = Buffer.concat([packet, data]);
-			this.sendPacket(0x6a, packet, debug);
-		}
-	
-		enterLearning() {
-			let packet = Buffer.alloc(16, 0);
-			packet[0] = 3;
-			this.sendPacket(0x6a, packet);
-		}
-	
-		checkTemperature() {
-			let packet = Buffer.alloc(16, 0);
-			packet[0] = 1;
-			this.sendPacket(0x6a, packet);
-		}
-	
-		cancelLearn() {
+
+		this.cancelRFSweep = () => {
 			const packet = Buffer.alloc(16, 0);
 			packet[0] = 0x1e;
 			this.sendPacket(0x6a, packet);
 		}
 	
-		addRFSupport() {
-			this.enterRFSweep = () => {
-				const packet = Buffer.alloc(16, 0);
-				packet[0] = 0x19;
-				this.sendPacket(0x6a, packet);
-			}
-	
-			this.checkRFData = () => {
-				const packet = Buffer.alloc(16, 0);
-				packet[0] = 0x1a;
-				this.sendPacket(0x6a, packet);
-			}
-	
-			this.checkRFData2 = () => {
-				const packet = Buffer.alloc(16, 0);
-				packet[0] = 0x1b;
-				this.sendPacket(0x6a, packet);
-			}
+		this.checkRFData = () => {
+			const packet = Buffer.alloc(16, 0);
+			packet[0] = 0x1a;
+			this.sendPacket(0x6a, packet);
 		}
-		*/
+	
+		this.checkRFData2 = () => {
+			const packet = Buffer.alloc(16, 0);
+			packet[0] = 0x1b;
+			this.sendPacket(0x6a, packet);
+		}
+	}
+	*/
+
+	// Event overloads with typed listener signatures
+	on(event: 'deviceReady', listener: (device: Device) => void): this;
+	on(event: 'temperature', listener: (temperature: number) => void): this;
+	on(event: string | symbol, listener: (...args: any[]) => void): this;
+	on(event: string | symbol, listener: (...args: any[]) => void): this {
+		return super.on(event, listener);
+	}
+}
+
+/**
+	 * Print a fomratted log message.
+	 * 
+	 * @param str The message to be displayed
+	 * @param component Optionally the message can be tagged with a component name or class name
+	 */
+function log(str: string, component: string | object | undefined = undefined): void {
+	let tag: string = '';
+	if (typeof component === 'string')
+		tag = ` \x1b[32m[${component}]`;
+	if (typeof component === 'object')
+		tag = ` \x1b[32m[${component.constructor.name}]`;
+	console.log(`\x1b[36m[${new Date().toISOString()}]${tag}\x1b[0m ${str}`);
 }
